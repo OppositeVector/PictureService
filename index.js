b0e64429dbff62ae22a6f1731621bac0c94846b9
niv7488 <niv7488@gmail.com>
2016-05-14 19:56:53 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 var http = require('http');
+var express = require('express');
+var app = express();
+
+app.use('/', express.static('./public'));
+
+var port = process.env.PORT || 3000;
+
+app.listen(port);
+console.log('listening on port ' + port);
