ec879a634df3b3b2bb8af1e236dc30efe8c80b2d
OppositeVector <iloriginal@gmil.com>
2016-05-26 11:10:46 +0300

    Knox testing

--- server.js
+++ server.js
@@ -1,26 +1,15 @@
 var http = require('http');
 var express = require('express');
-// var opsworks = require('opsworks');
-// var AWS = require("aws-sdk");
+var opsworks = require('opsworks');
+var AWS = require("aws-sdk");
 
-// var knoxClient = require('knox').createClient({
-// 	key: process.env.S3_KEY,
-// 	secret: process.env.S3_SECRET,
-// 	bucket: 'vicspicturestorage'
-// });
+var knoxClient = require('knox').createClient({
+	key: process.env.S3_KEY,
+	secret: process.env.S3_SECRET,
+	bucket: 'vicspicturestorage'
+});
 
-// var object = { foo: "bar" };
-// var string = JSON.stringify(object);
-// var req = knoxClient.put('/test/obj.json', {
-//     'Content-Length': Buffer.byteLength(string)
-//   , 'Content-Type': 'application/json'
-// });
-// req.on('response', function(res){
-//   if (200 == res.statusCode) {
-//     console.log('saved to %s', req.url);
-//   }
-// });
-// req.end(string);
+
 
 function GetDateTime() {
 
@@ -53,6 +42,23 @@ app.use('/', express.static('./public'));
 
 app.get('/pictures/:id', function(req, res) {
 	var picId = req.params.id;
+});
+
+app.get('/TestKnox' function(req, res) {
+
+	var object = { foo: "bar" };
+	var string = JSON.stringify(object);
+	var req = knoxClient.put('/test/obj.json', {
+	    'Content-Length': Buffer.byteLength(string)
+	  , 'Content-Type': 'application/json'
+	});
+	req.on('response', function(res){
+	  if (200 == res.statusCode) {
+	    console.log('saved to %s', req.url);
+	  }
+	});
+	req.end(string);
+
 });
 
 var port = process.env.PORT || 3000;
